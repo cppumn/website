0c04790662c3d88a5d27edeafba6384322e61f48
AbdeI1 <abdelrahmanmmokbel@gmail.com>
2022-07-26 16:26:34 -0500

    moved header and footer to main app

--- cpp-website/src/App.tsx
+++ cpp-website/src/App.tsx
@@ -1,14 +1,21 @@
+import UofMHeader from "components/UofM/UofMHeader"
+import UofMFooter from "components/UofM/UofMFooter"
+
 import MainPage from "pages/MainPage"
 
 import { HashRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <Router basename="/">
-      <Routes>
-        <Route path='/' element={ <MainPage /> } />
-      </Routes>
-    </Router>
+    <div>
+      <UofMHeader />
+      <Router basename="/">
+        <Routes>
+          <Route path='/' element={ <MainPage /> } />
+        </Routes>
+      </Router>
+      <UofMFooter />
+    </div>
   )
 }
 
--- cpp-website/src/pages/MainPage.tsx
+++ cpp-website/src/pages/MainPage.tsx
@@ -1,29 +1,22 @@
-import UofMHeader from "components/UofM/UofMHeader"
-import UofMFooter from "components/UofM/UofMFooter"
-
 import "css/MainPage.css"
 
 const MainPage = () => {
   return (
-    <div>
-      <UofMHeader />
-      <div className="page">
-        <h2 className="page-title"> Welcome to the Competitive Programming Club (CPP) </h2>
-        <div className="page-text">
-          <p>
-            We're a student group at the University of Minnesota interested in competitive programming.
-          </p>
-          <p>
-            Our goal is to create an environment where students can hone their speed and accuracy in solving challenging problems, learn new programming concepts, and prepare for competitions and interviews.
-          </p>
-          <p>
-            Join our Discord to attend meetings and keep up to date!
-          </p>
-        </div>
-        <h2 className="discord-link">UMN.CPP Discord</h2>
-        <iframe src="https://discord.com/widget?id=646499730343460864&theme=dark" width="350" height="500" style={{border: '0px'}} sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts" title="Discord widget"></iframe>
+    <div className="page">
+      <h2 className="page-title"> Welcome to the Competitive Programming Club (CPP) </h2>
+      <div className="page-text">
+        <p>
+          We're a student group at the University of Minnesota interested in competitive programming.
+        </p>
+        <p>
+          Our goal is to create an environment where students can hone their speed and accuracy in solving challenging problems, learn new programming concepts, and prepare for competitions and interviews.
+        </p>
+        <p>
+          Join our Discord to attend meetings and keep up to date!
+        </p>
       </div>
-      <UofMFooter />
+      <h2 className="discord-link">UMN.CPP Discord</h2>
+      <iframe src="https://discord.com/widget?id=646499730343460864&theme=dark" width="350" height="500" style={{border: '0px'}} sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts" title="Discord widget"></iframe>
     </div>
   )
 }
