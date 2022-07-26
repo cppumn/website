edf4be49f171a92f1c5c86093f0b43ac39a423c9
AbdeI1 <abdelrahmanmmokbel@gmail.com>
2022-07-26 15:33:56 -0500

    route updates

--- cpp-website/src/App.tsx
+++ cpp-website/src/App.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path='/' element={ <MainPage /> } />
         <Route path='/header' element={ <UofMHeader /> } />
