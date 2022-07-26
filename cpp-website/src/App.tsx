414b0049e63fd5644cede2a4b516ca0f69fe04fc
AbdeI1 <abdelrahmanmmokbel@gmail.com>
2022-07-26 16:07:22 -0500

    router works

--- cpp-website/src/App.tsx
+++ cpp-website/src/App.tsx
@@ -1,14 +1,12 @@
 import MainPage from "pages/MainPage"
-import UofMHeader from "components/UofM/UofMHeader"
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router basename="/">
       <Routes>
         <Route path='/' element={ <MainPage /> } />
-        <Route path='/header' element={ <UofMHeader /> } />
       </Routes>
     </Router>
   )
