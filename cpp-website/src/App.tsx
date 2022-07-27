1dc0260fcff7bf52f22629c1f6f633150cdc25cd
AbdeI1 <abdelrahmanmmokbel@gmail.com>
2022-07-26 22:26:28 -0500

    hax

--- cpp-website/src/App.tsx
+++ cpp-website/src/App.tsx
@@ -2,16 +2,18 @@ import UofMHeader from "components/UofM/UofMHeader"
 import UofMFooter from "components/UofM/UofMFooter"
 
 import MainPage from "pages/MainPage"
+import RoomPage from "pages/RoomPage"
 
-import { HashRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <div>
       <UofMHeader />
-      <Router basename="/">
+      <Router basename={process.env.PUBLIC_URL} >
         <Routes>
           <Route path='/' element={ <MainPage /> } />
+          <Route path='/room' element={ <RoomPage /> } />
         </Routes>
       </Router>
       <UofMFooter />
---
+++ cpp-website/src/pages/RoomPage.tsx
@@ -0,0 +1,7 @@
+const RoomPage = () => {
+  return (
+    <h1> You are now in the room page </h1>
+  )
+}
+
+export default RoomPage
