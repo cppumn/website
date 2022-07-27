9525b81907577cd6f4ee90723b7077419d9c1a97
AbdeI1 <abdelrahmanmmokbel@gmail.com>
2022-07-27 15:24:23 -0500

    added script for multiple htmls

--- cpp-website/bin/build.sh
+++ cpp-website/bin/build.sh
@@ -1,2 +1,7 @@
 npm run build
 cp build/index.html build/404.html
+
+## if we need seperate html files:
+# ROUTES=$(grep -oP "(?<=<Route path='/)[A-z]*" src/App.tsx)
+# FILES=$(echo ${ROUTES} | sed 's/[A-z]*/build\/&.html/g')
+# for f in ${FILES}; do cp build/index.html ${f}; done
