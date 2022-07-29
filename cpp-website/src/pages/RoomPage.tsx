e4e2401f4ab049f5c599047e5f131c598a36bacc
TheRoyalMuffinMan <totalhockeyman@gmail.com>
2022-07-28 21:27:25 -0500

    Adding Chakra-Icons and Removing unused files

--- cpp-website/src/pages/RoomPage.tsx
+++ cpp-website/src/pages/RoomPage.tsx
@@ -1,17 +1,17 @@
-import { useEffect } from "react"
-import { useLocation } from "react-router-dom"
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 const RoomPage = () => {
-  const location: any = useLocation()
-  const msg: string = location.state ? location.state.msg : "you did not come from the link on the main page"
+  const location: any = useLocation();
+  const msg: string = location.state ? location.state.msg : "you did not come from the link on the main page";
 
   useEffect(() => {
-    console.log(msg)
-  }, [])
+    console.log(msg);
+  }, []);
 
   return (
     <h1> You are now in the room page </h1>
-  )
+  );
 }
 
-export default RoomPage
+export default RoomPage;
