e485324ada8b41e6aa9bba99202c16dc49f92463
Abdel <abdelrahmanmmokbel@gmail.com>
2022-07-29 23:28:20 -0500

    sticky footer and removed css

--- cpp-website/src/App.tsx
+++ cpp-website/src/App.tsx
@@ -2,11 +2,11 @@ import UofMHeader from "components/UofM/UofMHeader";
 import UofMFooter from "components/UofM/UofMFooter";
 
 import MainPage from "pages/MainPage";
-import RoomPage from "pages/RoomPage";
 import PageNotFound from "pages/PageNotFound";
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { chakra } from "@chakra-ui/system";
+import { Box } from "@chakra-ui/react";
 
 function App() {
   return (
@@ -14,11 +14,12 @@ function App() {
       <Router basename={process.env.PUBLIC_URL} >
         <UofMHeader />
         <Routes>
-          <Route path='/' element={ <MainPage /> } />
-          <Route path='/room' element={ <RoomPage /> } />
-          <Route path='/*' element={ <PageNotFound /> } />
+            <Route path='/' element={ <MainPage /> } />
+            <Route path='/*' element={ <PageNotFound /> } />
         </Routes>
-        <UofMFooter />
+        <Box top="100vh" position="sticky">
+          <UofMFooter />
+        </Box>
       </Router>
     </chakra.div>
   );
