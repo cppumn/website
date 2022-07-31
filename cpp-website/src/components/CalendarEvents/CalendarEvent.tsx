d48679ae8bef5975f646ee08aace7307ec021786
Abdel <abdelrahmanmmokbel@gmail.com>
2022-07-31 16:07:53 -0500

    better time formatting

--- cpp-website/src/components/CalendarEvents/CalendarEvent.tsx
+++ cpp-website/src/components/CalendarEvents/CalendarEvent.tsx
@@ -6,14 +6,23 @@ import { CalendarIcon } from "@chakra-ui/icons";
 
 const CalendarEvent = ({event}: {event : calendarEvent}) => {
 
+  const options: any = {
+    year: '2-digit',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit'
+  };
+
   const get12hourTimeString = (day: Date) => {
-    return `${day.getHours()%12 || 12}:${day.getMinutes()}:${day.getSeconds()} ${day.getHours() < 12 ? "AM" : "PM"}`
+    return Intl.DateTimeFormat('en-us', options).format(day).split(" ").slice(1).join(" ");
   }
   
 
   return (
     <Flex justifyContent="left" w="100%">
-      <CalendarIcon maxW={140} maxH={140} paddingRight={5} color={CPPTheme.blue}></CalendarIcon>
+      <CalendarIcon w={[0, 140]} h={[0, 140]} paddingRight={5} color={CPPTheme.blue}></CalendarIcon>
       <Flex flexDir="column">
         <Heading color={CPPTheme.lightblue}> {event.summary} </Heading>
         <Text> {event.description} </Text>
