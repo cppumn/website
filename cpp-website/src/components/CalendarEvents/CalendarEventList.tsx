1c832324465bf30f75056d498617e270137e9ce0
Abdel <abdelrahmanmmokbel@gmail.com>
2022-08-01 02:38:38 -0500

    description processes html

--- cpp-website/src/components/CalendarEvents/CalendarEventList.tsx
+++ cpp-website/src/components/CalendarEvents/CalendarEventList.tsx
@@ -1,18 +1,18 @@
 import CalendarEvent from "./CalendarEvent";
 
 import type { calendarEvent } from "services/GoogleCalendarApi";
-import { Divider, Flex } from "@chakra-ui/react";
+import { Box, Divider, Flex } from "@chakra-ui/react";
 import { CPPTheme } from "globals/CPPInfo";
 
 const CalendarEventList = ({events}: {events: calendarEvent[]}) => {
   return (
-    <Flex flexDir="column" gap={5} w="100%">
+    <Flex flexDir="column" w="100%">
       { events.length > 0 ? <CalendarEvent key={events[0].id} event={events[0]}/> : undefined}
       {events.slice(1).map(e => {return (
-        <>
-          <Divider borderColor={CPPTheme.blue} borderWidth={2}/>
-          <CalendarEvent key={e.id} event={e} />
-        </>
+        <Box key={e.id}>
+          <Divider borderColor={CPPTheme.blue} borderWidth={2} marginBottom={5} marginTop={5}/>
+          <CalendarEvent event={e} />
+        </Box>
       )})}
     </Flex>
   )
