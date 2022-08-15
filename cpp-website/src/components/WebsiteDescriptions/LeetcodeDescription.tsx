d01d9163a2fa67946abb3e7d93cff3484c68ab9d
Abdel <abdelrahmanmmokbel@gmail.com>
2022-08-15 01:54:50 -0500

    added summaries

--- cpp-website/src/components/WebsiteDescriptions/LeetcodeDescription.tsx
+++ cpp-website/src/components/WebsiteDescriptions/LeetcodeDescription.tsx
@@ -9,7 +9,11 @@ const LeetcodeDescription = () => {
       logo={logo} 
       url={url}
       title="LeetCode"
-      text="resource..."
+      text="
+        Perhaps the most well known of these resources, LeetCode is the most begginer friendly.
+        Its built-in text editor includes syntax highlighting and lets you run your code on sample test cases so an IDE isn't even required (though it is still recommended).
+        Furthermore, LeetCode also hosts seperate weekly and biweekly contests open to everyone.
+      "
     />
   );
 };
