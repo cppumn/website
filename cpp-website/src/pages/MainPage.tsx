3732de86e19cba4eac83eec84ca01e11db23695b
Abdel <abdelrahmanmmokbel@gmail.com>
2022-08-30 03:17:44 -0500

    in-progress: scroll down notification

--- cpp-website/src/pages/MainPage.tsx
+++ cpp-website/src/pages/MainPage.tsx
@@ -1,16 +1,36 @@
-import { useRef } from "react";
-import { Box, Flex } from "@chakra-ui/react";
+import { useState, useEffect, useRef } from "react";
+import { Box, Flex, Text } from "@chakra-ui/react";
 import Hero from "components/Home/Hero/Hero";
+import ScrollDownMessage from "components/Home/ScrollDownMessage";
 import Mission from "components/Home/Mission";
 import About from "components/Home/About/About";
 import GetInvolved from "components/Home/GetInvolved";
 import Banner from "components/Home/Banner";
 
 const MainPage = () => {
+
   const aboutRef = useRef<any>(null);
+  const atTop = () => {return window.scrollY == 0}
+  const [showMessage, setShowMessage] = useState(atTop());
+
+  let updateShowMessage = () => {
+    setShowMessage(atTop())
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', updateShowMessage);
+    
+    return () => window.removeEventListener('scroll', updateShowMessage);
+  },[]);
+
   return (
     <Flex flexDir="column" m="auto">
       <Hero scrollToRef={aboutRef}/>
+      {showMessage ?
+        <ScrollDownMessage />
+        :
+        undefined
+      }
       <Mission />
       <About containerRef={aboutRef}/>
       <GetInvolved />
